4c090fb82058c03dbbd31638f15fbfb2cea902a9
Kyaw Bo Bo Aung <kbba700@gmail.com>
2025-02-02 11:48:00 +0400

    raw content, splitting component and og meta tag

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="/assets/footer-img.jpg" />
+      <section>
+        <article>
+          <h4>Doormat Navigation</h4>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Reservation</a>
+            </li>
+            <li>
+              <a href="#">Order Online</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h4>Contact</h4>
+          <ul>
+            <li>
+              <a href="#">Address</a>
+            </li>
+            <li>
+              <a href="#">Phone number</a>
+            </li>
+            <li>
+              <a href="#">Email</a>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h4>Social Media Link</h4>
+          <ul>
+            <li>
+              <a href="#">Address</a>
+            </li>
+            <li>
+              <a href="#">Phone number</a>
+            </li>
+            <li>
+              <a href="#">Email</a>
+            </li>
+          </ul>
+        </article>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </main>
+  );
+};
+
+export default Main;
