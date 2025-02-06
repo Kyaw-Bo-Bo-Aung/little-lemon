a04577487bd2a710d4ec1ae7864bbd201b53aab7
Kyaw Bo Bo Aung <kbba700@gmail.com>
2025-02-06 23:35:52 +0400

    tdd and accessibility

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,139 +1,99 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-const BookingForm = ({ availableTimes, onDateChange, onSubmit }) => {
-  const [formData, setFormData] = useState({
-    date: "",
-    time: "",
-    guests: "1",
-    occasion: "Birthday",
+const BookingForm = ({ availableTimes, onDateChange = () => {}, onSubmit }) => {
+  const {
+    register,
+    setValue,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      date: "",
+      time: "",
+      guests: "1",
+      occasion: "Birthday",
+    },
   });
-  const [errors, setErrors] = useState();
 
-  const handleChange = (e) => {
+  const handleDateChange = (e) => {
     const { name, value } = e.target;
-    if(name === 'guests' && !Number(value)) {
-      return;
-    }
-    setFormData((prev) => ({ ...prev, [name]: value }));
-
-    if (name === "date") {
-      onDateChange(value);
-    }
+    setValue("date", value);
+    onDateChange(value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const { date, time, guests, occasion } = formData;
-    const errors = new Set();
-    if(!date) {
-      errors.add({
-        field: "date",
-        validations: [
-          {
-            rule: "required",
-            message: "Date field is required."
-          }
-        ]
-      })
-    }
-    if(!time) {
-      errors.add({
-        field: "time",
-        validations: [
-          {
-            rule: "required",
-            message: "Time field is required."
-          }
-        ]
-      })
-    }
-    if(!guests) {
-      errors.add({
-        field: "guests",
-        validations: [
-          {
-            rule: "required",
-            message: "Date field is required."
-          }
-        ]
-      })
-    }
-    if(!occasion) {
-      errors.add({
-        field: "occasion",
-        validations: [
-          {
-            rule: "required",
-            message: "Occasion field is required."
-          }
-        ]
-      })
-    }
-    if(errors.size > 0) {
-      setErrors(errors);
-      return;
-    }
-    onSubmit(formData);
+  const handleSubmitForm = (data, event) => {
+    onSubmit(data);
   };
 
   return (
     <>
-    <h1>Book Now</h1>
-      <form onSubmit={handleSubmit} className="booking-form">
-        <label htmlFor="res-date">Choose date</label>
+      <h1>Book Now</h1>
+      <form onSubmit={handleSubmit(handleSubmitForm)} className="booking-form">
+        <label htmlFor="date">Choose date</label>
         <input
+          {...register("date", {
+            required: { value: true, message: "Please select date." },
+            onChange: handleDateChange,
+          })}
+          id="date"
           type="date"
-          id="res-date"
-          name="date"
-          value={formData.date}
-          onChange={handleChange}
           required
         />
+        {errors.date && (
+          <span className="error-msg">{errors.date.message}</span>
+        )}
 
         <label htmlFor="res-time">Choose time</label>
         <select
           id="res-time"
-          name="time"
-          value={formData.time}
-          onChange={handleChange}
+          {...register("time", {
+            required: { value: true, message: "Please select time." },
+          })}
           required
         >
-          <option value="">Select a time</option>
+          <option>Select a time</option>
           {availableTimes?.map((time) => (
             <option key={time} value={time}>
               {time} (Available)
             </option>
           ))}
         </select>
+        {errors.time && (
+          <span className="error-msg">{errors.time.message}</span>
+        )}
 
         <label htmlFor="guests">Number of guests</label>
         <input
           type="number"
-          id="guests"
-          name="guests"
-          min="1"
-          max="10"
-          value={formData.guests}
-          onChange={handleChange}
-          required
+          {...register("guests", {
+            required: { value: true, message: "Please fill guest field." },
+            min: { value: 1, message: "Please choose at least 1 guest." },
+            max: {
+              value: 10,
+              message: "Our guests limit is at most 10 people",
+            },
+            valueAsNumber: true,
+          })}
         />
+        {errors.guests && (
+          <span className="error-msg">{errors.guests.message}</span>
+        )}
 
         <label htmlFor="occasion">Occasion</label>
         <select
           id="occasion"
-          name="occasion"
-          value={formData.occasion}
-          onChange={handleChange}
+          {...register("occasion", {
+            required: { value: true, message: "Please select occasion." },
+          })}
         >
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
-
-        <input
-          type="submit"
-          value="Book"
-          className="submit-btn"
-        />
+        {errors.occasion && (
+          <span className="error-msg">{errors.occasion.message}</span>
+        )}
+        <input className="submit-btn" type="submit" value="Book" aria-label="On Click" />
       </form>
     </>
   );
